07ca6c369985b70971343d05651dd96dc385ce53
rickelmeribeiro <157402554+rickelmeribeiro@users.noreply.github.com>
2024-04-04 13:56:11 -0300

    ATT

--- git/BotaoCadastrar.php
+++ git/BotaoCadastrar.php
@@ -1,1 +1,4 @@
-<button type="button" class="btn btn-success btn-sm float-end">Cadastrar</button>
+<button type="button" class="btn btn-success btn-sm float-end" data-bs-toggle="modal"
+                data-bs-target="#modalAddUsuario"
+                onclick="abrirModalJsCliente('ID','IDDOINPUT','NOMEMODAL','A','ADD EDIT EXC','NOMEFORMULARIO')">Cadastrar
+        </button>
