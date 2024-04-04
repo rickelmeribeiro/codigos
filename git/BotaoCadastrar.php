f54e6bbe09684d626da1b144ee95df7ae4a3e701
rickelmeribeiro <157402554+rickelmeribeiro@users.noreply.github.com>
2024-04-04 13:34:46 -0300

    att

--- git/BotaoCadastrar.php
+++ git/BotaoCadastrar.php
@@ -1,3 +1,1 @@
-<button type="button" class="btn btn-success btn-sm float-end" data-bs-toggle="modal"
-        onclick="abrirModalJsCliente('ID','IDDONINPUT', 'NOMEDAMODAL', 'A', 'ADD OU EDIT OU EXC', 'NOMEFORMULARIO')">Cadastrar
-    <span class="mdi mdi-database-plus"></span></button>
+<button type="button" class="btn btn-success btn-sm float-end">Cadastrar</button>
